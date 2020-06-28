a63ceb0dac32bf60d0cecd0427c69c719dc751d6
Federall <bender92@list.ru>
2020-06-28 22:34:36 +0300

    sc-44 Create @Repository interface for Orders

--- src/main/java/com/example/onlinelibrary/controller/PublisherController.java
+++ src/main/java/com/example/onlinelibrary/controller/PublisherController.java
@@ -1,4 +1,51 @@
 package com.example.onlinelibrary.controller;
 
+import com.example.onlinelibrary.model.Publisher;
+import com.example.onlinelibrary.repository.PublisherRepository;
+import org.springframework.http.converter.json.GsonBuilderUtils;
+import org.springframework.web.bind.annotation.*;
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/publisher")
 public class PublisherController {
+    PublisherRepository publisherRepository;
+
+    public PublisherController(PublisherRepository publisherRepository) {
+        this.publisherRepository = publisherRepository;
+    }
+
+    @GetMapping("/findall")
+    public List<Publisher> findAllPublisher(){
+        return publisherRepository.findAll();
+    }
+
+    @GetMapping("/findbyid/{id}")
+    public List <Publisher> findById(@PathVariable int id){
+        return publisherRepository.findById(id);
+    }
+
+    @GetMapping("findbyname/{name}")
+    public Publisher findByname(@PathVariable String name) {
+        return publisherRepository.findByName(name);
+    }
+
+    @GetMapping("findbycontact/{contact}")
+    public Publisher findByContact(@PathVariable String contact) {
+        return  publisherRepository.findByContact(contact);
+    }
+
+    @PostMapping
+    public Publisher createPublisher(@RequestBody Publisher publisher) {return publisherRepository.save(publisher);}
+
+    @DeleteMapping("/{id}")
+    public String deletePublisherById(@PathVariable int id){
+        publisherRepository.deleteById(id);
+        return "Deleted publisher with id: " + id;
+    }
+
+
+
 }
--- src/main/java/com/example/onlinelibrary/repository/PublisherRepository.java
+++ src/main/java/com/example/onlinelibrary/repository/PublisherRepository.java
@@ -1,4 +1,14 @@
 package com.example.onlinelibrary.repository;
 
-public interface PublisherRepository {
+import com.example.onlinelibrary.model.Publisher;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public interface PublisherRepository extends JpaRepository<Publisher, Integer> {
+    List<Publisher> findById(int id);
+    Publisher findByName(String name);
+    Publisher findByContact(String contact);
+
 }
