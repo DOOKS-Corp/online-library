c8730d0e9179375bf7c42a0f5741676c64405ce6
olglk <olgalukianchukk@gmail.com>
2020-07-02 22:24:47 +0300

    SC-43 created customer repository and 6 custom methods

--- src/main/java/com/example/onlinelibrary/repository/CustomerRepository.java
+++ src/main/java/com/example/onlinelibrary/repository/CustomerRepository.java
@@ -1,9 +1,20 @@
 package com.example.onlinelibrary.repository;
 
+import com.example.onlinelibrary.model.Address;
 import com.example.onlinelibrary.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+
+    public Customer findByLastName(String lastName);
+    public Customer findByEmail(String email);
+    public List<Customer> findByAddresses(Address address);
+    public List<Customer> findByAddresses_zipCode(String zipCode);
+    public List<Customer> findByAddresses_state(String state);
+    public List<Customer> findByAddresses_city(String city);
+
 }
