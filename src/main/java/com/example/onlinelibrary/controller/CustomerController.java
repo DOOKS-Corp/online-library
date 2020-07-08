d51c0fab30d161e0b1fd0e491e13eb78bae8e61f
olglk <olgalukianchukk@gmail.com>
2020-07-08 17:03:24 +0300

    first 2 methods of Customer Controller test class

--- src/main/java/com/example/onlinelibrary/controller/CustomerController.java
+++ src/main/java/com/example/onlinelibrary/controller/CustomerController.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 @RestController
-@RequestMapping("/api/customer")
+@RequestMapping("/customer")
 public class CustomerController {
 
     CustomerRepository customerRepository;
